eccc198a0cc35c85d931f798411d6d61847f05f6
torbenconto <torbenmconto@gmail.com>
2025-07-30 15:55:43 -0400

    Allow all origins for more flexibility with self hosting

--- backend/cmd/server/routes.go
+++ backend/cmd/server/routes.go
@@ -19,7 +19,7 @@ func SetupRoutes(db *gorm.DB) (*gin.Engine, error) {
 
 	// CORS config
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://*", "https://*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
