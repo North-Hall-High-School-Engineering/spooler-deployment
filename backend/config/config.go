a7452bc8d390f27df83ea0aea5cd5896962ae902
torbenconto <torbenmconto@gmail.com>
2025-08-04 19:59:36 -0400

    Refactor error handling in LoadConfig function

--- backend/config/config.go
+++ backend/config/config.go
@@ -60,12 +60,13 @@ func LoadConfig(path string) (*Config, error) {
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	var cfg Config
-
 	if err := viper.ReadInConfig(); err != nil {
-		return nil, err
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return nil, err
+		}
 	}
 
+	var cfg Config
 	if err := viper.Unmarshal(&cfg); err != nil {
 		return nil, err
 	}
